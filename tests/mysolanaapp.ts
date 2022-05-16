18e9f624caefc3e68fb917cf64a0f57974efbbe3
Davi Banfi <davibanfi1579@outlook.com>
2022-05-16 14:05:28 -0300

    feat: add react app

--- tests/mysolanaapp.ts
+++ tests/mysolanaapp.ts
@@ -1,16 +1,47 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+import { assert } from "chai";
 import { Mysolanaapp } from "../target/types/mysolanaapp";
+const { SystemProgram } = anchor.web3;
 
 describe("mysolanaapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.local();
+
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Mysolanaapp as Program<Mysolanaapp>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  let baseAccount: anchor.web3.Keypair;
+  before(async () => {
+    baseAccount = anchor.web3.Keypair.generate()
+  })
+
+  it("Creates a counter)", async () => {
+    /* Call the create function via RPC */
+    await program.rpc.create({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
+
+    /* Fetch the account and check the value of count */
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 0: ', account.count.toString())
+    assert.ok(account.count.toString() === "0");
+  });
+
+  it("Increments the counter", async () => {
+    await program.rpc.increment({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Count 1: ', account.count.toString())
+    assert.ok(account.count.toString() === "1");
   });
 });
